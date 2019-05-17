83968a53f8ae0c5dcc01c612a562f103db16c8bc
Ricardo Alfredo Macias Olvera <ra_mo116@hotmail.com>
2019-05-17 10:31:24 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,37 +4,15 @@ import java.util.HashMap;
 
 public class Main {
 	
-	
-	  public static void main(String[] args) {
-		  
-		 
-		  
+	  public static void main(String[] args) {		  		 		 
 		    Main.func("Hello world!");
 		  }
 	
 	  public static void func(String str) {
-		    String output = "";
-		    HashMap<Object, Object> map = new HashMap<Object, Object>();
-		  	
-		  int j =0;  
-		    for (int i = 0; i < str.length(); i++) {
-		      char character = str.charAt(i);		  
-		      map.put(character, character);
-		     
-		/*     if (output.indexOf(character) == -1){
-		        output += character;
-		     //   map.put(character, character);
-		      }*/
-		      
-		      j++;
+		    HashMap<Object, Object> map = new HashMap<Object, Object>();  
+		    for (int i = 0; i < str.length(); i++) {				  
+		      map.put(str.charAt(i), str.charAt(i));		      		   
 		    }
-		    System.out.println(output);
-		    System.out.println("map size "+map.size());
 		   System.out.println("map -> "+ map);
-		    	
-		    	
-		   
-		    System.out.println("only one loop -> "+ j);
 		  }
-
 }
