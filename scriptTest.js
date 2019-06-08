9841b5c120de28283d8d72ff998e91342299013d
ricardo <ra_mo116@hotmail.com>
2019-06-08 16:41:26 -0700

    Fix dynamic array

--- scriptTest.js
+++ scriptTest.js
@@ -1,21 +1,18 @@
 
 //Ricardo Alfredo Macias Olvera
 
-var arr =[1,23, [1, [ 2, [1] , 2]], [1]];
-console.log("Input -> "+arr);
-var myData = [];
-recu(arr);
-console.log("Output -> "+ myData);
-function recu(arr){
-   
-    for(let i =0; i<arr.length; i++){
-        if (typeof arr[i] == 'object'){
-            recu(arr[i]);
-        }else{
-           myData.push(arr[i]); 
+arr =[1,23, [1, [ 2, [1] , 2]], [1]];
+console.log("Input -> ", arr);
+
+var vec =[];
+vec = funcTest(arr,vec);
+console.log("Output-> " , vec);
+function funcTest(arr,vec){
+    for(var i = 0 ; i < arr.length; i++){
+        if (typeof arr[i] == 'number'){
+            vec.push(arr[i]);
         }
-        
+        funcTest(arr[i],vec);
     }
-
+return vec;
 }
-
