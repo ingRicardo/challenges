d2f827788a9e5897833eb0d8bd77ce0958066cbc
ramo28 <ra_mo116@hotmail.com>
2019-06-13 08:43:13 -0700

    *Fix

--- scriptTest.js
+++ scriptTest.js
@@ -1,21 +1,26 @@
 
 //Ricardo Alfredo Macias Olvera
 
-var arr =[1,23, [1, [ 2, [1] , 2]], [1]];
-console.log("Input -> "+arr);
-var myData = [];
-recu(arr);
-console.log("Output -> "+ myData);
-function recu(arr){
-   
-    for(let i =0; i<arr.length; i++){
-        if (typeof arr[i] == 'object'){
-            recu(arr[i]);
-        }else{
-           myData.push(arr[i]); 
-        }
-        
-    }
+arrTypes =["1",23, ["1", [ 2, [1] , 2]], [1.3]];
+console.log("Input ",arrTypes);
+var vecTypes = [];
+function testType ( arr, vecTypes ){
 
+    for (var i=0; i < arr.length; i++){
+      
+            if (typeof arr[i] == 'object' || Array.isArray(arr[i]))
+                testType(arr[i], vecTypes);
+            if (typeof arr[i] == 'string'){
+                var n = parseInt(arr[i],10);              
+                vecTypes.push(n);            
+            }
+            if (typeof arr[i] == 'number'){             
+                vecTypes.push(arr[i]);             
+            }
+
+    }
+return vecTypes;
 }
 
+vecTypes = testType(arrTypes, vecTypes);
+console.log("Output ",vecTypes);
