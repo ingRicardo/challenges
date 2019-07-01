72e68cc3ac18de5c7e01486058eac77f676c23b7
ramo28 <ra_mo116@hotmail.com>
2019-07-01 09:43:31 -0700

    new array inside function

--- scriptTest.js
+++ scriptTest.js
@@ -1,26 +1,55 @@
 
 //Ricardo Alfredo Macias Olvera
 
-arrTypes =["1",23, ["1", [ 2, [1] , 2]], [1.3]];
-console.log("Input ",arrTypes);
-var vecTypes = [];
+var arrStart =["1",23, ["1", [ 2, [1] , 2]], [1.3]];
+console.log("input -> ", arrStart);
+var VecRes = arrayTest(arrStart)
+console.log("output -> ",  VecRes);
+
+function arrayTest(arrStart){
+   var res = [];
+   var vecTypes = [];
+   for (var i=0; i < arrStart.length; i++){
+    
+    if ( Array.isArray(arrStart[i])){
+               
+        vecTypes=   testType(arrStart[i], vecTypes);
+
+    }else if (typeof arrStart[i] == 'string'){
+        var n = parseInt(arrStart[i],10);              
+        res.push(n);            
+    } else if (typeof arrStart[i] == 'number'){             
+        res.push(arrStart[i]);             
+    } 
+
+   }
+   for (var i=0; i < vecTypes.length; i++){
+ 
+    res.push(vecTypes[i]);
+    } 
+   
+   return res;
+}
+
+
 function testType ( arr, vecTypes ){
 
     for (var i=0; i < arr.length; i++){
       
-            if (typeof arr[i] == 'object' || Array.isArray(arr[i]))
+            if ( Array.isArray(arr[i])){
                 testType(arr[i], vecTypes);
+            }
+                
             if (typeof arr[i] == 'string'){
                 var n = parseInt(arr[i],10);              
                 vecTypes.push(n);            
             }
             if (typeof arr[i] == 'number'){             
                 vecTypes.push(arr[i]);             
-            }
+            }  
+            
     
     }
 return vecTypes;
 }
 
-vecTypes = testType(arrTypes, vecTypes);
-console.log("Output ",vecTypes);
